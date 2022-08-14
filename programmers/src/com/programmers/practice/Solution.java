648557bc4e53795bc46458af2ba529216af96166
nsj9331 <nsj9331@gmail.com>
2022-08-14 12:04:10 +0900

    Revert "lev1. 정수 내림차순으로 배치하기"
    This reverts commit 64800576f9e92eadc98ef04ac97fe2a857fee0dd.

--- programmers/src/com/programmers/practice/Solution.java
+++ programmers/src/com/programmers/practice/Solution.java
@@ -1,32 +1,13 @@
 package com.programmers.practice;
-import java.util.Arrays;
-public class Solution {
 
-	public static void main(String[] args) {
-		
-		 	int n = 118372 ; 
-		 	
-		      String newN = String.valueOf(n); // n을 String으로 변환 
-		      
-		      int[] arr = new int[newN.length()]; // String으로 변환한 n을 담을 int배열 선언
-		      
-		        for(int i = 0; i<arr.length; i++){
-		            arr[i] = (newN.charAt(i)) -'0'; //String을 char로 변환 후 정수만 추출후 int 배열에 담기
-		        }
-		        
-		        Arrays.sort(arr); // 배열을 오름차순 정렬
-		        
-		        String str = "";
-		        
-		        for(int i  = arr.length-1;  i>=0; i--){
-		            str += arr[i]; //배열을 다시 내림차순 정렬 후 String으로 변환 , 내림차순 정렬시에는 배열길이 마지막부터 0까지 
-		            				//넣어준다.
-		        }
-		        
-		        Long answer = Long.parseLong(str);// 내림차순 정렬된 배열을 Long으로 변환
-		        
-		        System.out.println(answer);
-		       
+class Solution {
+	public double solution(int[] arr) {
+		double sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+
+		}
+		return sum / arr.length;
 	}
 
 }
