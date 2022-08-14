734f42cad06680c7cc82fc88d6c47acb5a4289a2
nsj9331 <nsj9331@gmail.com>
2022-08-14 11:38:28 +0900

    lev1 . 정수를 반대로 뒤집어서 배열에 넣기

--- programmers/src/com/programmers/practice/Solution.java
+++ programmers/src/com/programmers/practice/Solution.java
@@ -1,32 +1,20 @@
 package com.programmers.practice;
-import java.util.Arrays;
-public class Solution {
-
-	public static void main(String[] args) {
+// 정수를 반대로 뒤집어서 배열에 넣기
+class Solution {
+	
+	public int[] solution(long n) {
 		
-		 	int n = 118372 ; 
-		 	
-		      String newN = String.valueOf(n); // n을 String으로 변환 
-		      
-		      int[] arr = new int[newN.length()]; // String으로 변환한 n을 담을 int배열 선언
-		      
-		        for(int i = 0; i<arr.length; i++){
-		            arr[i] = (newN.charAt(i)) -'0'; //String을 char로 변환 후 정수만 추출후 int 배열에 담기
-		        }
-		        
-		        Arrays.sort(arr); // 배열을 오름차순 정렬
-		        
-		        String str = "";
-		        
-		        for(int i  = arr.length-1;  i>=0; i--){
-		            str += arr[i]; //배열을 다시 내림차순 정렬 후 String으로 변환 , 내림차순 정렬시에는 배열길이 마지막부터 0까지 
-		            				//넣어준다.
-		        }
-		        
-		        Long answer = Long.parseLong(str);// 내림차순 정렬된 배열을 Long으로 변환
-		        
-		        System.out.println(answer);
-		       
-	}
+		String newN = String.valueOf(n);
+		int[] answer = new int[newN.length()];
+		int len = answer.length;
 
+		int index = 0;
+		
+		for (int i = len - 1; i >= 0; i--) { // answer 배열을 반대로 리버스 
+			answer[index] = newN.charAt(i) - '0'; // int 를 정수로 변환 48을 빼주어도됨.
+			index++;
+		}
+		return answer;
+
+	}
 }
